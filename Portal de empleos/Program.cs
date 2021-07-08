15dcf3df245be08c71f0188dc81a68fcfa20dece
Justin Jr <77954884+JustinLuuu@users.noreply.github.com>
2021-07-08 16:10:06 -0400

    console writeline

--- Portal de empleos/Program.cs
+++ Portal de empleos/Program.cs
@@ -18,10 +18,10 @@ namespace Portal_de_empleos
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
